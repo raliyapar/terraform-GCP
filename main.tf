872905cc557c1da4579d3f5221c0cf37fa9b1a4e
Raliya Parveen <Raliya.Parveen+kyndryl@kyndryl.com>
2026-03-03 23:46:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@ provider "google" {
 }
 
 resource "google_project_service" "compute" {
-  service = "compute.googleapis.com"
+  service            = "compute.googleapis.com"
   disable_on_destroy = false
-  
 }
 
 resource "google_storage_bucket" "my_bucket" {
   name     = "${var.project_id}-tf-bucket"
-  location = var.region  
+  location = var.region
+
+  uniform_bucket_level_access = true
 }
